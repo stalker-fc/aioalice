2be5a5cadd4919387f2325b77ccb7332eb5b5bce
sergey <Stepanov.S@digdes.com>
2018-12-12 15:33:14 +0300

    add alice for creation wellfields in geoclient

---
+++ geoclient/http_client.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import json
+import base64
+from typing import Optional
+from geoclient import API_URL
+import multiprocessing as mp
+
+from requests import request as make_request
+
+#: Словарь HTTP-заголовков для каждого запроса к API
+HEADERS = {'Content-Type': 'application/json'}
+
+
+def get(url, params=None, **kwargs):
+    return request('get', url, params=params, **kwargs)
+
+
+def post(url, data=None, json=None, **kwargs):
+    return request('post', url, data=data, json=json, **kwargs)
+
+
+def put(url, data=None, **kwargs):
+    return request('put', url, data=data, **kwargs)
+
+
+def delete(url, data=None, **kwargs):
+    return request('delete', url, data=data, **kwargs)
+
+
+def request(method, url, **kwargs):
+    # check URL relativity
+    if 'http://' not in url:
+        url = API_URL + url
+
+    # init headers with default 'application/json' if None given
+    if 'headers' not in kwargs:
+        kwargs['headers'] = {}
+
+    kwargs['headers'].update(HEADERS)
+
+    # use no 'Content-Type' for files upload as it is stated in requests as default parameter
+    if 'files' in kwargs and kwargs['files'] and 'Content-Type' in kwargs['headers']:
+        del kwargs['headers']['Content-Type']
+
+    return make_request(method, url, **kwargs)
+
+
+def get_app_versions_from_geoclient():
+    res = get('/available_app_versions').json()
+    if res['status'] == 200:
+        return res['items']
+    else:
+        raise ValueError
+
+
+def get_wellfields_from_geoclient():
+    res = get('/available_wellfields').json()
+    if res['status'] == 200:
+        return res['items']
+    else:
+        raise ValueError
+
+
+def is_wellfield_exist(app_version, prefix, name):
+    res = post('/is_wellfield_exist', data=json.dumps(dict(app_version=app_version, name=name, prefix=prefix))).json()
+    if res['status'] == 200:
+        return res['exist']
+    else:
+        raise ValueError
+
+
+def _create_wellfield_in_separate_process(app_version, prefix, name):
+    if os.fork() != 0:
+        return
+    post('/create_wellfield', data=json.dumps(dict(app_version=app_version, name=name, prefix=prefix))).json()
+    sys.exit(0)
+
+
+def create_wellfield(app_version, prefix, name):
+    p = mp.Process(target=_create_wellfield_in_separate_process, args=(app_version, prefix, name))
+    p.start()
---
+++ geoclient/wellfield.py
@@ -0,0 +1,157 @@
+import logging
+
+from aiohttp import web
+from aioalice import Dispatcher, get_new_configured_app, types
+from aioalice.dispatcher import MemoryStorage
+from aioalice.utils.helper import Helper, HelperMode, Item
+from geoclient.http_client import get_app_versions_from_geoclient, get_wellfields_from_geoclient, is_wellfield_exist, create_wellfield
+WEBHOOK_URL_PATH = '/'  # webhook endpoint
+
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 1337
+
+
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
+
+# Создаем экземпляр диспетчера и подключаем хранилище в памяти
+dp = Dispatcher(storage=MemoryStorage())
+
+YES = 'Да'
+NO = 'Нет'
+YES_NO = [YES, NO]
+
+# Можно использовать класс Helper для хранения списка состояний
+class CreationState(Helper):
+    mode = HelperMode.snake_case
+
+    SELECT_APP_VERSION = Item()
+    SELECT_WELLFIELD = Item()
+    INSERT_PREFIX = Item()
+    CONFIRM_WELLFIELD_CREATION = Item()
+    CREATE_WELLFIELD = Item()
+
+
+
+def get_app_versions():
+    return get_app_versions_from_geoclient()
+
+
+def get_wellfields():
+    return get_wellfields_from_geoclient()
+
+
+@dp.request_handler(contains=['отмена'])
+async def cancel_operation(alice_request):
+    user_id = alice_request.session.user_id
+    await dp.storage.reset_state(user_id)
+    return alice_request.response('Хорошо, прекращаем.')
+
+
+@dp.request_handler(state=CreationState.SELECT_APP_VERSION, contains=get_app_versions())
+async def select_app_version(alice_request):
+    user_id = alice_request.session.user_id
+    await dp.storage.update_data(user_id, app_version=alice_request.request.command)
+    text = 'Отлично! Теперь выбери месторождение которое ты хочешь создать.'
+    await dp.storage.set_state(user_id, CreationState.SELECT_WELLFIELD)
+    return alice_request.response(text, buttons=get_wellfields())
+
+
+@dp.request_handler(state=CreationState.SELECT_APP_VERSION)
+async def select_app_version_not_in_list(alice_request):
+    return alice_request.response(
+        'Такой версии приложения у нас нет :(\nВыбери одну из списка!',
+        buttons=get_app_versions()
+    )
+
+
+@dp.request_handler(state=CreationState.SELECT_WELLFIELD, contains=get_wellfields())
+async def select_wellfield(alice_request):
+    user_id = alice_request.session.user_id
+    await dp.storage.update_data(user_id, wellfield=alice_request.request.command)
+    text = 'Теперь назови имя того, для кого нужно создать месторождение.'
+    await dp.storage.set_state(user_id, CreationState.INSERT_PREFIX)
+    return alice_request.response(text)
+
+
+@dp.request_handler(state=CreationState.SELECT_WELLFIELD)
+async def select_wellfield_not_in_list(alice_request):
+    return alice_request.response(
+        'Такого месторождения не существует :(\nВыбери одно из списка!',
+        buttons=get_app_versions()
+    )
+
+
+@dp.request_handler(state=CreationState.INSERT_PREFIX)
+async def insert_prefix(alice_request):
+    user_id = alice_request.session.user_id
+    await dp.storage.update_data(user_id, prefix=alice_request.request.command)
+    data = await dp.storage.get_data(user_id)
+    text = f'Вы точно хотите создать месторождение:\n "[ {data["prefix"]} ] {data["wellfield"]}" \nна {data["app_version"]}-версии приложения?'
+    await dp.storage.set_state(user_id, CreationState.CONFIRM_WELLFIELD_CREATION)
+    return alice_request.response(text, buttons=['Да', 'Нет'])
+
+
+@dp.request_handler(state=CreationState.CONFIRM_WELLFIELD_CREATION, contains=YES_NO)
+async def insert_prefix(alice_request):
+    user_id = alice_request.session.user_id
+    command = alice_request.request.command
+    buttons = []
+    if command == 'Да':
+        data = await dp.storage.get_data(user_id)
+        if is_wellfield_exist(data["app_version"], data["prefix"], data["wellfield"]):
+            text = 'Это месторождение уже существует в базе данных. Создать его заново?'
+            await dp.storage.set_state(user_id, CreationState.CREATE_WELLFIELD)
+            buttons=YES_NO
+        else:
+            text = 'Начинаем инициализировать месторождение...\n Ожидаемое время инициализации 7-10 минут.'
+            await dp.storage.reset_state(user_id)
+    else:
+        text = 'Не инициализируем месторождение.'
+        await dp.storage.reset_state(user_id)
+
+    return alice_request.response(text, buttons=buttons)
+
+@dp.request_handler(state=CreationState.CONFIRM_WELLFIELD_CREATION)
+async def insert_prefix(alice_request):
+    return alice_request.response(
+        'Я тебя не поняла :( \nСкажи да или нет.',
+        buttons=YES_NO
+    )
+
+@dp.request_handler(state=CreationState.CREATE_WELLFIELD, contains=YES_NO)
+async def insert_prefix(alice_request):
+    user_id = alice_request.session.user_id
+    command = alice_request.request.command
+    await dp.storage.reset_state(user_id)
+    if command == 'Да':
+        data = await dp.storage.get_data(user_id)
+        create_wellfield(data["app_version"], data["prefix"], data["wellfield"])
+        text = 'Начинаем инициализировать месторождение...\n Ожидаемое время инициализации 7-10 минут.'
+    else:
+        text = 'Не инициализируем месторождение.'
+
+    return alice_request.response(text)
+
+@dp.request_handler(state=CreationState.CREATE_WELLFIELD)
+async def insert_prefix(alice_request):
+    return alice_request.response(
+        'Я тебя не поняла :( \nСкажи да или нет.',
+        buttons=YES_NO
+    )
+
+@dp.request_handler()
+async def handle_any_request(alice_request):
+    user_id = alice_request.session.user_id
+    await dp.storage.set_state(user_id, CreationState.SELECT_APP_VERSION)
+    text = 'Я умею создавать месторождения! На какой версии прототипа это будем делать?'
+    # Если сессия новая, приветствуем пользователя
+    if alice_request.session.new:
+        text = 'Привет! ' + text
+    # Предлагаем пользователю список игр
+    return alice_request.response(text, buttons=get_app_versions())
+
+
+if __name__ == '__main__':
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
